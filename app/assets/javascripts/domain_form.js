6068452abd5455483112fd84c8161f36b63a0553
Lenin Luque <xleninx@gmail.com>
2017-05-31 16:55:49 -0400

    add examples

--- app/assets/javascripts/domain_form.js
+++ app/assets/javascripts/domain_form.js
@@ -1,4 +1,10 @@
 $(function(){
+  $('.link-url').on('click', function(ev){
+    ev.preventDefault();
+    $('#domain').val($(this).children('.url').text());
+    console.log($(this).children('.url').text())
+  });
+
   $('#call-json').on('click', function(){
     $.get('/domain_status/by_domain.json', {domain: $('#domain').val()}, function(data){
       $('.json-request').html('<h3>Status: '+data['status']+'</h3>')
